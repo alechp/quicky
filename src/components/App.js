73ea8a52a75fe55fb1e43e048c75324c45c62209
Alec Hale-Pletka <alec@alechp.com>
2016-05-24 11:25:12 -0700

    Linter working now. Had to add babel-eslint dependency

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,26 @@
-import React, { Component , PropTypes} from 'react';
+import React, { Component, PropTypes} from 'react';
 import CSSModules from 'react-css-modules';
 import style from './style/app-style.scss';
 
 
 const propTypes = {
-    name: React.PropTypes.string
+  name: React.PropTypes.string,
 };
 const defaultProps = {
-    name: 'World'
+  name: 'World',
 };
 
 class App extends Component {
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return (
-            <div className="app">
+  constructor (props) {
+    super(props);
+  }
+  render () {
+    return (
+            <div className='app'>
                 <h1>Hello {this.props.name}</h1>
             </div>
-        )
-    }
+        );
+  }
 }
 
 App.propTypes = propTypes;
