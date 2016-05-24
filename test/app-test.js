be5ce4ebc4d20be41ddfbbe246cfd2d6a6d082e4
Alec Hale-Pletka <alec@alechp.com>
2016-05-24 12:01:27 -0700

    Updated paths from src to app for current test and old reference file

--- test/app-test.js
+++ test/app-test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import App from '../src/components/App';
+import App from '../app/components/App';
 
 describe('<App />', () => {
    it('app is app', () => {
