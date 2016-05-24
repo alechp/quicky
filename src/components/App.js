8c6ee6199aa466c9fbc0cc8a89feaa38aa9e81c7
Alec Hale-Pletka <alec@alechp.com>
2016-05-24 11:52:35 -0700

    Fixed linter errors. Need to run tests on build next

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
-import React, { Component, PropTypes} from 'react';
-import CSSModules from 'react-css-modules';
+import React, { Component } from 'react';
 import style from './style/app-style.scss';
-
 
 const propTypes = {
   name: React.PropTypes.string,
@@ -11,13 +9,10 @@ const defaultProps = {
 };
 
 class App extends Component {
-  constructor (props) {
-    super(props);
-  }
   render () {
     return (
-            <div className='app'>
-                <h1>Hello {this.props.name}</h1>
+            <div className={style.app}>
+                <h1>{`Hello ${this.props.name}`}</h1>
             </div>
         );
   }
